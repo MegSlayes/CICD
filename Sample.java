405ab85bb688db6e19701a1bbd68136fb876f368
Methun01 <muralikamalesh18@gmail.com>
2026-02-20 00:28:17 +0530

    added changes

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample{
     public static void main(String[] args) {
         System.out.print("Its a CICD demo");
-        System.out.println("Added with updated script");
+        System.out.println("Added with the updated script");
     }
 }
